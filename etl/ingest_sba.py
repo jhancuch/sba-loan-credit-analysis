82654b1a45a6ba0a7205eb4374a8452df20e4106
Joe Hancuch <joseph.hancuch1@gmail.com>
2022-07-15 13:16:35 -0400

    locate url for .csv file

--- etl/ingest_sba.py
+++ etl/ingest_sba.py
@@ -1,9 +1,19 @@
 import logging 
 
-def download(year: str):
+import re
+
+def download(year: int):
     """
     Obtains the url for the .csv file containing sba loan data and returns the data as an object
-    year (string): The files are split up by decade, e.g. "2010", "2020"
+    year (int): The files are split up by decade, e.g. 2010 or 2020
     """
 
-    logging
+    logging.info('Requesting data for 10 year increment starting in {}.'.format(year))
+
+    REGEX_STRING = 'r"http.*7afy' + str(year) + '-fy' + str(year + 9) + '.*csv"'
+
+    url_string = re.search(REGEX_STRING)
+
+
+
+
